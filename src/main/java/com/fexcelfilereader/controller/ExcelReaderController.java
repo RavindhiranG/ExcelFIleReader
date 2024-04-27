410fa169570aaba8b6f5d04bf392cd2d5f14f83d
RavindhiranG <ravindhirang@gmail.com>
2024-04-27 12:42:27 +0530

    Second commit

--- src/main/java/com/fexcelfilereader/controller/ExcelReaderController.java
+++ src/main/java/com/fexcelfilereader/controller/ExcelReaderController.java
@@ -1,30 +1,27 @@
 package com.fexcelfilereader.controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 @RestController
 public class ExcelReaderController {
     
-	@PostMapping("/read-excel")
+	@GetMapping("/read-excel")
 	public List<List<String>> readExcelFile() throws IOException {
 	    String filePath = "C:/Users/Ravindhiran/workspace-spring-tool-suite-4-4.20.1.RELEASE/ExcelFIleReader/sample.xlsx";
 	    // Read the Excel file
 	    return readExcelData(filePath);
 	}
-	private List<List<String>> readExcelData(String filePath) throws IOException {
+	public List<List<String>> readExcelData(String filePath) throws IOException {
 	    List<List<String>> data = new ArrayList<>();
 
 	    FileInputStream inputStream = new FileInputStream(filePath);
